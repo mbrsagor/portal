94f73f8e5ded99a712f37aaaa7ab1c4cc8fdb3ef
mbrsagor <mbrsagor@gmail.com>
2020-06-05 16:44:00 +0600

    MobProject config done

---
+++ src/components/common/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import style from '../../style/style.scss'
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="main_header">
+                <h2>hello I am here...</h2>
+            </div>
+        );
+    }
+}
+
+export default Header;
+
+
