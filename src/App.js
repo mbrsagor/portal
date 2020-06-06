a7336b6ae86a98dfc512ea4c7c3152ac685deb80
mbrsagor <mbrsagor@gmail.com>
2020-06-06 10:24:45 +0600

    SCSS console bug fixed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import style from '../src/style/style.scss';
+import '../src/style/style.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { Component } from 'react'
 import { BrowserRouter, Switch, Route } from "react-router-dom";
