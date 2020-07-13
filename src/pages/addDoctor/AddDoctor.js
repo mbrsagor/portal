15b97b85f35a329c5312a5a5cc5e279deb5c100b
mbrsagor <mbrsagor@gmail.com>
2020-07-13 14:46:29 +0600

    Doctor view ready with form

--- src/pages/addDoctor/AddDoctor.js
+++ src/pages/addDoctor/AddDoctor.js
@@ -1,39 +1,183 @@
-import React, {Component} from 'react';
-import {Row, Col} from 'react-bootstrap'
+import React, { Component } from 'react';
+import { Row, Col } from 'react-bootstrap'
 import PageTitle from '../../components/common/PageTitle';
 import Header from '../../components/common/Header';
 import Footer from '../../components/common/Footer';
 import Sidebar from '../../components/common/Sidebar';
 
 class AddDoctor extends Component {
-    state = {  }
-    render() { 
+    state = {}
+    render() {
         return (
             <>
-            <Header />
-            <Row className="fix_height m-0">
-                <Col className="sidebar_bg_color p-0" lg={2}>
-                    <Sidebar />
-                </Col>
-                <Col className="p-0" lg={10}>
-                    <PageTitle title="Add Doctor" sub_title="Add New Doctor" />
-                    <div className="addDoctorForm">
-                        <form>
-                            <Row>
-                                <Col lg={6}>
-                                    <div className="form-group">
-
-                                    </div>
-                                </Col>
-                            </Row>
-                        </form>
-                    </div>
-                </Col>
-            </Row>
-            <Footer />
+                <Header />
+                <Row className="fix_height m-0">
+                    <Col className="sidebar_bg_color p-0" lg={2}>
+                        <Sidebar />
+                    </Col>
+                    <Col className="p-0" lg={10}>
+                        <PageTitle title="Add Doctor" sub_title="Add New Doctor" />
+                        <div className="addDoctorForm p-4 m-4 bg-light">
+                            <form>
+                                <Row>
+                                    <Col lg={6}>
+                                        <div className="form-group">
+                                            <label htmlFor="specialization">Doctor Specialization</label>
+                                            <input type="text"
+                                                className="form-control"
+                                                id="specialization"
+                                                ref="specialization"
+                                                name="specialization"
+                                                placeholder="Enter doctor specialization"
+                                            />
+                                            {/* <small className="text-danger">{this.state.specialization}</small> */}
+                                        </div>
+                                        <div className="form-group">
+                                            <label htmlFor="visit_fee">Doctor Visit Fee</label>
+                                            <input type="text"
+                                                className="form-control"
+                                                id="visit_fee"
+                                                ref="visit_fee"
+                                                name="visit_fee"
+                                                placeholder="Enter doctor visit fee"
+                                            />
+                                            {/* <small className="text-danger">{this.state.visit_fee}</small> */}
+                                        </div>
+                                        <div className="form-group">
+                                            <label htmlFor="designation">Doctor Designation</label>
+                                            <input type="text"
+                                                className="form-control"
+                                                id="designation"
+                                                ref="designation"
+                                                name="designation"
+                                                placeholder="Enter doctor designation"
+                                            />
+                                            {/* <small className="text-danger">{this.state.designation}</small> */}
+                                        </div>
+                                        <div className="form-group">
+                                            <label htmlFor="roles">Select Roles</label>
+                                            <select
+                                                className="form-control"
+                                                id="address"
+                                                ref="address"
+                                                name="address">
+                                                <option value="1">Admin</option>
+                                                <option value="2">Nurse</option>
+                                                <option value="3">Doctor</option>
+                                            </select>
+                                        </div>
+                                        <div className="form-group">
+                                            <label htmlFor="desiexperiencegnation">Doctor Experience</label>
+                                            <input type="text"
+                                                className="form-control"
+                                                id="experience"
+                                                ref="experience"
+                                                name="experience"
+                                                placeholder="Enter work experience"
+                                            />
+                                            {/* <small className="text-danger">{this.state.experience}</small> */}
+                                        </div>
+                                        <div className="form-group">
+                                            <label htmlFor="gender">Select Gender</label>
+                                            <select
+                                                className="form-control"
+                                                id="gender"
+                                                ref="gender"
+                                                name="gender">
+                                                <option value="1">Male</option>
+                                                <option value="2">Female</option>
+                                                <option value="3">Others</option>
+                                            </select>
+                                        </div>
+                                    </Col>
+                                    <Col lg={6}>
+                                        <div className="form-group">
+                                            <label htmlFor="address">Doctor Address</label>
+                                            <input type="text"
+                                                className="form-control"
+                                                id="address"
+                                                ref="address"
+                                                name="address"
+                                                placeholder="Enter doctor address"
+                                            />
+                                            {/* <small className="text-danger">{this.state.address}</small> */}
+                                        </div>
+                                        <div className="form-group">
+                                            <label htmlFor="phone_number">Phone Number</label>
+                                            <input type="tel"
+                                                className="form-control"
+                                                id="phone_number"
+                                                ref="phone_number"
+                                                name="phone_number"
+                                                placeholder="Enter phone number"
+                                            />
+                                            {/* <small className="text-danger">{this.state.phone_number}</small> */}
+                                        </div>
+                                        <div className="form-group">
+                                            <label htmlFor="phone_number">Date Of Birth</label>
+                                            <input type="date"
+                                                className="form-control"
+                                                id="date_of_birth"
+                                                ref="date_of_birth"
+                                                name="date_of_birth"
+                                                placeholder="Enter date of birth"
+                                            />
+                                            {/* <small className="text-danger">{this.state.date_of_birth}</small> */}
+                                        </div>
+                                        <div className="form-group">
+                                            <label htmlFor="department">Select Department</label>
+                                            <select
+                                                className="form-control"
+                                                id="department"
+                                                ref="department"
+                                                name="department">
+                                                <option value="1">Admin</option>
+                                                <option value="2">Nurse</option>
+                                                <option value="3">Doctor</option>
+                                            </select>
+                                        </div>
+                                        <div className="form-group">
+                                            <label htmlFor="availability">Select Availability</label>
+                                            <select
+                                                className="form-control"
+                                                id="availability"
+                                                ref="availability"
+                                                name="availability">
+                                                <option value="1">Friday</option>
+                                                <option value="2">Sunday</option>
+                                                <option value="3">Monday</option>
+                                            </select>
+                                        </div>
+                                        <br />
+                                        <div className="form-group">
+                                            <div className="custom-file">
+                                                <label className="custom-file-label" htmlFor="profile_photo">Profile photo (Optional)</label>
+                                                <input type="file"
+                                                    name="profile_photo"
+                                                    ref="profile_photo"
+                                                    className="custom-file-input"
+                                                    id="profile_photo"
+                                                />
+                                            </div>
+                                        </div>
+                                    </Col>
+                                </Row>
+                                <textarea
+                                    className="form-control"
+                                    id="education"
+                                    ref="education"
+                                    name="education"
+                                    placeholder="Enter all education"
+                                />
+                                <button className="btn btn-success mt-3" type="submit">Save</button>
+                            </form>
+                        </div>
+                    </Col>
+                </Row>
+                <Footer />
             </>
         );
     }
 }
- 
+
 export default AddDoctor;
