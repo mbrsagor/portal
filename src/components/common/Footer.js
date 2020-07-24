de72409393785acb00037df5d6dedef63c863907
Sagor <mbrsagor@gmail.com>
2020-07-24 21:10:20 +0600

    Copy wirte text colo change

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -5,7 +5,7 @@ class Footer extends Component {
         return (
             <footer className="pt-2 pb-0 ftr_custom">
                 <div className="text-center">
-                    <p className="text-black">&copy; All right reserved || Develop by <a href="https://www.github.com/mbrsagor">Mbr-Sagor</a></p>
+                    <p className="text-light">&copy; All right reserved || Develop by <a href="https://www.github.com/mbrsagor"><b>Mbr-Sagor</b></a></p>
                 </div>
             </footer>
         );
