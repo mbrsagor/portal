2e7d4c026d08fb8a40ec8f0b249ae9bec1983806
mbrsagor <mbrsagor@gmail.com>
2024-06-10 23:06:17 +0600

    New: Install new app.

--- portal/settings.py
+++ portal/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "order", # New app
+    "order",  # New app
 ]
 
 MIDDLEWARE = [
