6107b0a8db3f09b3358da92183ecbe32694f817a
mbrsagor <mbrsagor@gmail.com>
2024-06-11 10:27:02 +0600

    New: Make utils for project structured.

---
+++ utils/messages.py
@@ -0,0 +1,15 @@
+from django.utils.translation import gettext as _
+
+SIGNIN_SUCCESS = _("Successfully user has been login.")
+SIGNIN_FAILED = _("Invalid login.")
+NOT_FOUND = _("No content found this ID.")
+DATA_RETURN = _("Data returned successfully.")
+OTP_VERIFY = _("Your OTP Verified success.")
+OTP_NOT_MATCH = _("Your Email OTP does not match.")
+SIGN_OUT_SUCCESS = _("Sign out successfully.")
+EXPIRED = _("Already token has been expired.")
+SIGNUP_SUCCESS = _("Successfully the account has been created. Please check your email we send OTP for active account.")
+OTP_SUCCESS = _("OTP send successfully to the email address.")
+PASSWORD_CHANGE = _("Password change successfully.")
+PASSWORD_MSG = _("Sorry! Password cant be empty.")
+
