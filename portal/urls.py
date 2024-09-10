500388ed71d281dcd1f0eaedc44fa85c8fdca563
dev-monir <monir.grb@gmail.com>
2024-09-10 13:16:00 +0600

    New: Added theme

--- portal/urls.py
+++ portal/urls.py
@@ -22,3 +22,11 @@ urlpatterns = [
     path('user/', include('user.urls')),
 ]
 
+# Load static files
+from django.conf import settings
+from django.conf.urls.static import static
+
+# Start load files & media
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
